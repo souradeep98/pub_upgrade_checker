a9dace2fb1af99de1da3627758f4bb36bf8713ab
souradeep98 <souradeepmondal414@gmail.com>
2022-09-25 10:16:07 +0530

    ...

--- lib/src/pages.dart
+++ lib/src/pages.dart
@@ -1,17 +1,6 @@
 library pages;
 
-import 'dart:async';
-import 'dart:io';
-import 'package:async/async.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart' hide Response;
-import 'package:grouped_list/grouped_list.dart';
-import 'package:pub_upgrade_checker/src/globals.dart';
-import 'package:pub_upgrade_checker/src/structures.dart';
-import 'package:pub_upgrade_checker/src/utils.dart';
-
-import 'package:flutter_essentials/flutter_essentials.dart';
 import 'package:pub_upgrade_checker/src/widgets.dart';
 
 part 'pages/app_frame.dart';
